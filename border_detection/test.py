5598a4c4355464cc7520824ea40cca5e28ec5df1
naomipark3 <naomi_park@brown.edu>
2023-06-16 15:37:52 -0700

    symbolic regression

--- border_detection/test.py
+++ border_detection/test.py
@@ -1,9 +1,10 @@
-from numpy import asarray
+import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 from astropy.io import fits
+from gplearn.genetic import SymbolicRegressor
 
-im = fits.open('/home/npark/Desktop/naomipark_mirsi/cal_jcf.043054.gz') #reads in fits file
+im = fits.open('/Users/naomipark/Desktop/jpl_internship/naomipark_mirsi/cal_jcf.043054.gz') #reads in fits file
 red_data = im[0].data #i believe this accesses pixel values of each image (not entirely sure what this means though???)
 
 print("shape: ", red_data.shape) #(277, 357)
@@ -18,6 +19,7 @@ for i in range(len(red_data[:, 200])):
     count += 1
     y_value.append(count)
 print("y_value: ", y_value)
+y_value = np.array(y_value)
 
 fig1 = plt.figure(1)
 plt.imshow(im[0].data)
@@ -32,3 +34,8 @@ plt.title('Pixel Value vs. Y Value')
 plt.legend()
 
 plt.show()
+for i in range(len(red_data[0])):
+    est = SymbolicRegressor(population_size=500, generations=10, tournament_size=20, stopping_criteria=0.01, verbose=1)
+    est.fit(y_value.reshape(-1,1), red_data[:, 50])
+    # equation = est.export()
+    print("best fit for column: ", i, ", ", est._program)
