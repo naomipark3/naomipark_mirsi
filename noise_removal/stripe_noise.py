0a4f5f64f9ab3b899678815f03099f723483edf5
naomipark3 <naomi_park@brown.edu>
2023-06-20 14:40:10 -0700

    total TV term

--- noise_removal/stripe_noise.py
+++ noise_removal/stripe_noise.py
@@ -3,7 +3,15 @@ from PIL import Image
 import matplotlib.pyplot as plt
 from astropy.io import fits
 
+im = fits.open('/Users/naomipark/Desktop/jpl_internship/naomipark_mirsi/cal_jcf.043054.gz') #reads in fits file
+red_data = im[0].data #i believe this accesses pixel values of each image (not entirely sure what this means though???)
+#data is in the form of I (erg/s/cm^2/ster/cm^-1)
+print("original image: ", red_data[:, 200])
 
+fig1 = plt.figure(1)
+plt.imshow(im[0].data)
+plt.title("Original Image")
+plt.show()
 
 def mean_column(image): #will take in "im" as argument
     return np.mean(image, axis=0)
@@ -12,18 +20,20 @@ def finite_difference_second_order(b):
     return np.pad(np.diff(b,2), (1,1), 'edge')
 
 # Euler-Lagrange PDE numerical approximator
-def correction(b_old, z, del_t, lambda_):
+def correction(b_old, z, del_t, lambda_, lambda_tv):
     z_xx = finite_difference_second_order(z)
     b_old_xx = finite_difference_second_order(b_old)
-    return b_old - del_t * (z_xx - b_old_xx + lambda_ * b_old) #see [Eq. 6] from article
+    tv_term = np.pad(np.diff(b_old), (1,0), 'constant')
+    return b_old - del_t * (z_xx - b_old_xx + lambda_ * b_old + lambda_tv * tv_term) #see [Eq. 6] from article (with TV term)
 
-def stripe_noise_correction(image, init_bias, del_t, niters, lambda_=0.00001):
-    z = mean_column(image) #returns a 1-D array where each entry is the MEAN of each col
+def stripe_noise_correction(image, init_bias, del_t, niters, lambda_=0.00001, lambda_tv=0.1):
+    z = mean_column(image)
     b = init_bias
-    for i in range(niters): #ok so this would be iterative and not recursive...
-        b = correction(b, z, del_t, lambda_)
-    corrected_image = image - b #see [Eq. 7]
-    return corrected_image, b #should return an array of pixel values if I am correct...?
+    for i in range(niters):
+        b = correction(b, z, del_t, lambda_, lambda_tv)
+    corrected_image = image - b
+    return corrected_image, b
+
 
 def normalize(image):
     return image / np.max(image)
@@ -31,29 +41,12 @@ def normalize(image):
 def denormalize(image, original_max):
     return (image * original_max).astype(original_max)
 
-def rescale_image(image):
-    min_val = np.min(image)
-    max_val = np.max(image)
-    rescaled_image = 255 * (image - min_val) / (max_val - min_val)
-    rescaled_image = rescaled_image.astype(np.uint8)
-    return rescaled_image
-
-im = fits.open('/Users/naomipark/Desktop/jpl_internship/naomipark_mirsi/cal_jcf.043054.gz') #reads in fits file
-red_data = im[0].data #i believe this accesses pixel values of each image (not entirely sure what this means though???)
-#data is in the form of I (erg/s/cm^2/ster/cm^-1)
-red_data = rescale_image(red_data)
-print("original image: ", red_data[:, 200])
-
-fig1 = plt.figure(1)
-plt.imshow(im[0].data)
-plt.title("Original Image")
-plt.show()
-
-# original_max = np.max(red_data)
+original_max = np.max(red_data)
+red_data = normalize(red_data) #i can't tell if normalization/denormalization is really necessary...
 
 initial_bias = np.zeros(red_data.shape[1]) #.shape returns a tuple that represents size, so .shape[1] helps us to access the columns
 corrected_image, estimated_bias = stripe_noise_correction(red_data, initial_bias, del_t=0.01, niters=1000) #set timestep equal to 0.1 and niters=100 for now
-# corrected_image = denormalize(corrected_image, original_max)
+corrected_image = denormalize(corrected_image, original_max)
 
 fig2 = plt.figure(2)
 plt.imshow(corrected_image)
