107b9d14d06621f703b462de1879a2af33f8605d
Naomi Park <naomi_park@brown.edu>
2023-06-22 08:51:42 -0700

    wjup added

--- README.md
+++
@@ -1,1 +0,0 @@
-# naomipark_mirsi
--- border_detection/fits_data.py
+++
@@ -1,5 +0,0 @@
-from astropy.io import fits
-import matplotlib.pyplot as plt
-import numpy as np
-
-im = fits.open('/prvt/juno1/mirsi/2023feb28/images/4.90/jupiter/wjup.00208.b.fits.gz')
