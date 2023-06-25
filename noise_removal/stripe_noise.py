f5caf2595b923a2161cc073a3c9024c9b4153b62
naomipark3 <naomi_park@brown.edu>
2023-06-24 21:19:12 -0700

    new-old

--- noise_removal/stripe_noise.py
+++ noise_removal/stripe_noise.py
@@ -5,73 +5,18 @@ import matplotlib.pyplot as plt
 from astropy.io import fits
 import alphashape
 
-im = fits.open('/Users/naomipark/Desktop/jpl_internship/naomipark_mirsi/cal_jcf.043054.gz') #reads in fits file
+im = fits.open('/Users/naomipark/Desktop/jpl_internship/naomipark_mirsi/wjup.00059.a.fits.gz') #reads in fits file
 red_data = im[0].data #i believe this accesses pixel values of each image (not entirely sure what this means though???)
 #data is in the form of I (erg/s/cm^2/ster/cm^-1)
 print("original image: ", red_data[:, 200])
 
 fig1 = plt.figure(1)
 plt.imshow(im[0].data)
-plt.savefig('jup_output.jpg')
 plt.title("Original Image")
 plt.show()
 
-gray_jpg = cv2.imread('/Users/naomipark/Desktop/jpl_internship/naomipark_mirsi/jup_output.jpg', cv2.IMREAD_GRAYSCALE)
-
-# cv2.imshow("gray jpg", gray_jpg)
-# cv2.waitKey(0)
-plt.imshow(gray_jpg, cmap='gray')
-plt.show()
-
-# Ensure the image was loaded properly
-if gray_jpg is None:
-    print("Failed to load image")
-else:
-    # Try to find circles in the image
-    detected_circle = cv2.HoughCircles(gray_jpg, cv2.HOUGH_GRADIENT, 145, 40, param1 = 30, param2 = 100, minRadius=50, maxRadius=150)
-    #changing num of votes doesn't do anything
-
-print(detected_circle)
-
-'''
-***GLOBAL VARS*** (not great syntax...)
-'''
-coordinate_list = []
-radius = 0
-if detected_circle is not None:
-  
-    # Convert the circle parameters a, b and r to integers.
-    detected_circles = np.uint16(np.around(detected_circle))
-
-    for pt in detected_circles[0, :]:
-        a, b, r = pt[0], pt[1], pt[2]
-        radius = r
-        coordinate_list.append((a,b))
-        # Draw the circumference of the circle.
-        cv2.circle(gray_jpg, (a, b), r, (0, 255, 0), 2)
-  
-        # Draw a small circle (of radius 1) to show the center.
-        cv2.circle(gray_jpg, (a, b), 1, (0, 0, 255), 3)
-        cv2.imshow("Detected Circle", gray_jpg)
-        cv2.waitKey(0)
-
-def mean_column(image, coordinate_list, radius): # Will take in "im" and "coordinate_list" as arguments
-    a, b = coordinate_list[0] # Extract the x, y coordinates and radius of the planet
-    r = radius
-    means = []
-    for j in range(image.shape[1]): # For each column
-        if a-r <= j <= a+r: # If the column contains part of the planet
-            upper_boundary = b - r
-            lower_boundary = b + r
-            above_planet = image[:upper_boundary, j] #slices image array to get all the rows above the upper boundary of the planet
-            below_planet = image[lower_boundary+1:, j]
-            mean = np.mean(np.concatenate([above_planet, below_planet])) #concatenate pixel values from above_planet and below_planet
-            #into a single array and calculate the mean of those values
-        else:
-            mean = np.mean(image[:, j])
-        means.append(mean)
-    return np.array(means)
-
+def mean_column(image): #will take in "im" as argument
+    return np.mean(image, axis=0)
 
 def finite_difference_second_order(b):
     return np.pad(np.diff(b,2), (1,1), 'edge')
@@ -100,7 +45,7 @@ def correction(b_old, z, del_t, lambda_):
 These biases are manifest as stripe noise. 
 '''
 def stripe_noise_correction(image, init_bias, del_t, niters, lambda_=0.01):
-    z = mean_column(image, coordinate_list, radius)
+    z = mean_column(image)
     b = init_bias
 
     '''in iteration, 'b' is updated to better estimate the bias of each column based on 'correction()'
