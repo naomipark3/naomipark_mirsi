0788d6dfac1d13830e66062c800fd269629d862e
naomipark3 <naomi_park@brown.edu>
2023-08-16 09:45:07 -0700

    cleaned

--- noise_removal/stripe_noise_directory.py
+++ noise_removal/stripe_noise_directory.py
@@ -107,11 +107,11 @@ def process_fits_file(file_path):
         initial_bias = estimated_bias
 
     #Show image after algorithm has been run x number of times
-    fig1 = plt.figure(1)
-    plt.imshow(corrected_image)
-    plt.title(f"Corrected Image (20X) - {file_path}")
-    plt.colorbar()
-    plt.show()
+    # fig1 = plt.figure(1)
+    # plt.imshow(corrected_image)
+    # plt.title(f"Corrected Image (20X) - {file_path}")
+    # plt.colorbar()
+    # plt.show()
 
     '''
     The following code that is commented out plots the difference between
@@ -130,7 +130,7 @@ def process_fits_file(file_path):
     # plt.show()
 
     # Define a new path for the corrected images
-    corrected_image_directory = '/Users/naomipark/Desktop/jpl_internship/naomipark_mirsi/june_22/18_40_corrected_images'
+    corrected_image_directory = '/Users/naomipark/Desktop/jpl_internship/naomipark_mirsi/aug_1/4_90_corrected_images'
     
     # Ensure the new directory exists, if not, create it
     if not os.path.exists(corrected_image_directory):
@@ -155,7 +155,7 @@ def process_fits_file(file_path):
 
 
 # Specify path of the directory that contains uncleaned images
-path = '/Users/naomipark/Desktop/jpl_internship/naomipark_mirsi/june_22/18_40'
+path = '/Users/naomipark/Desktop/jpl_internship/naomipark_mirsi/aug_1/4_90'
 
 # Get list of all .fits.gz files in the directory
 fits_files = [f for f in os.listdir(path) if f.endswith('.fits.gz')]
